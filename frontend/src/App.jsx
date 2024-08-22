8acfa6a0bd48760748b80f873ac9d2e015f9936d
trick0846 <trick0846@naver.com>
2024-08-22 20:56:38 +0900

    feat: page 및 path 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,46 @@
 import "./index.css";
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+// import { useState } from "react";
 
-  return <div></div>;
+import MainPage from "./pages/Main/MainPage";
+import RankPage from "./pages/Rank/RankPage";
+import CalendarPage from "./pages/Calendar/CalendarPage";
+import SearchPage from "./pages/Search/SearchPage";
+import DetailPage from "./pages/Detail/DetailPage";
+import RafflePage from "./pages/Raffle/RafflePage";
+import RaffleDetailPage from './pages/Raffle/RaffleDetailPage';
+import ProfilePage from "./pages/Profile/ProfilePage";
+function App() {
+  return (
+    <RecoilRoot>
+      <Router>
+        <div className="mx-auto flex max-w-[450px] justify-center">
+          <div className="min-h-dvh w-full">
+            <Routes>
+              {/* 메인페이지 */}
+              <Route exact path="/" element={<MainPage />} />
+              {/* 인기페이지 */}
+              <Route path="/rank" element={<RankPage />} />
+              {/* 캘린더페이지 */}
+              <Route path="/calendar" element={<CalendarPage />} />
+              {/* 검색페이지 */}
+              <Route path="/search" element={<SearchPage />} />
+              {/* 상세페이지 */}
+              <Route path="/detail/:id" element={<DetailPage />} />
+              {/* 래플 전체페이지 */}
+              <Route path="/raffle" element={<RafflePage />} />
+              {/* 래플 상세페이지 */}
+              <Route path="/raffle/:raffleId" element={<RaffleDetailPage />} />
+              {/* 마이페이지 */}
+              <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+          </div>
+          {/* <NavigationBar /> */}
+        </div>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
 export default App;
