d6038dcbfc3f137693a26da90b640e366ce994d2
Mingyeong Ji <t01045239155@gmail.com>
2024-08-24 23:05:34 +0900

    feat : axios test

--- frontend/src/pages/Search/ImageGrid.jsx
+++ frontend/src/pages/Search/ImageGrid.jsx
@@ -1,50 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import image1 from '../../../public/SearchImg/1.png';
-import image2 from '../../../public/SearchImg/2.png';
-import image3 from '../../../public/SearchImg/3.jpg';
-import image4 from '../../../public/SearchImg/4.jpg';
-import image5 from '../../../public/SearchImg/5.jpeg';
-import image6 from '../../../public/SearchImg/6.jpg';
-import image7 from '../../../public/SearchImg/7.jpg';
-import image8 from '../../../public/SearchImg/8.jpg';
-import image9 from '../../../public/SearchImg/9.jpg';
-import image10 from '../../../public/SearchImg/10.png';
-import image11 from '../../../public/SearchImg/11.jpg';
-import image12 from '../../../public/SearchImg/12.jpg';
-import image13 from '../../../public/SearchImg/13.png';
-import image14 from '../../../public/SearchImg/14.png';
-import image15 from '../../../public/SearchImg/15.jpg';
+import axios from 'axios';
 
 const ImageGrid = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [images, setImages] = useState([]);
   const [filteredImages, setFilteredImages] = useState([]);
   const navigate = useNavigate();
 
-  const imageData = [
-    { id: 1, imageUrl: image1, title: "Image Title 1", popUpStoreNo: "123" },
-    { id: 2, imageUrl: image2, title: "Image Title 2", popUpStoreNo: "124" },
-    { id: 3, imageUrl: image3, title: "Image Title 3", popUpStoreNo: "125" },
-    { id: 4, imageUrl: image4, title: "Image Title 4", popUpStoreNo: "126" },
-    { id: 5, imageUrl: image5, title: "Image Title 5", popUpStoreNo: "127" },
-    { id: 6, imageUrl: image6, title: "Image Title 6", popUpStoreNo: "128" },
-    { id: 7, imageUrl: image7, title: "Image Title 7", popUpStoreNo: "129" },
-    { id: 8, imageUrl: image8, title: "Image Title 8", popUpStoreNo: "130" },
-    { id: 9, imageUrl: image9, title: "Image Title 9", popUpStoreNo: "131" },
-    { id: 10, imageUrl: image10, title: "Image Title 10", popUpStoreNo: "132" },
-    { id: 11, imageUrl: image11, title: "Image Title 11", popUpStoreNo: "133" },
-    { id: 12, imageUrl: image12, title: "Image Title 12", popUpStoreNo: "134" },
-    { id: 13, imageUrl: image13, title: "Image Title 13", popUpStoreNo: "135" },
-    { id: 14, imageUrl: image14, title: "Image Title 14", popUpStoreNo: "136" },
-    { id: 15, imageUrl: image15, title: "Image Title 15", popUpStoreNo: "137" },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/v1/store/list');
+        console.log(response.data)
+        const data = Array.isArray(response.data) ? response.data : []; // 배열인지 확인 후 설정
+        setImages(data);
+        setFilteredImages(data);
+        console.log(data)
+      } catch (error) {
+        console.error("Error fetching the data", error);
+      }
+    };
+    
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    const filtered = imageData.filter(item =>
+    const filtered = images.filter(item =>
       item.popUpStoreNo.includes(searchTerm)
     );
     setFilteredImages(filtered);
-  }, [searchTerm]);
+  }, [searchTerm, images]);
 
   const handleImageClick = (id) => {
     navigate(`/detail/${id}`);
@@ -70,7 +56,7 @@ const ImageGrid = () => {
               onClick={() => handleImageClick(item.id)}
             >
               <img
-                src={item.imageUrl}
+                src={item.storeThumbnailUrl}
                 alt={item.title}
                 className="w-full h-full object-cover rounded-sm"
               />
