9fd67da6444d1b53345b26ae68064e5be13bd467
Mingyeong Ji <t01045239155@gmail.com>
2024-08-24 16:25:11 +0900

    feat: pwa 적용 확인, test시 build 필요

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,33 +6,46 @@ import path from 'path';
 export default defineConfig({
   plugins: [
     react(),
-  //   VitePWA({
-  //     registerType: 'autoUpdate',
-  //     includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
-  //     manifest: {
-  //       name: 'My Vite PWA App',
-  //       short_name: 'VitePWA',
-  //       description: 'A PWA built with Vite and React',
-  //       theme_color: '#ffffff',
-  //       icons: [
-  //         {
-  //           src: 'pwa-192x192.png',
-  //           sizes: '192x192',
-  //           type: 'image/png'
-  //         },
-  //         {
-  //           src: 'pwa-512x512.png',
-  //           sizes: '512x512',
-  //           type: 'image/png'
-  //         }
-  //       ]
-  //     }
-  //   })
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        "name": "My Vite PWA App",
+        "short_name": "VitePWA",
+        "description": "A PWA built with Vite and React",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone",
+        "start_url": "/",
+        "icons": [
+          {
+            "src": "logo308.png",  // 아이콘 경로 설정
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "logo308.png",  // 동일 파일을 다른 크기로 사용
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+        "screenshots": [
+          {
+            "src": "/screenshot1.png",
+            "sizes": "540x720",
+            "type": "image/png"
+          }
+        ]
+      }
+      
+    })
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-
+  server: {
+    open: true, // 개발 서버 실행 시 브라우저 자동 열기
+  },
 });
