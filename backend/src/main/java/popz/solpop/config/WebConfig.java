06f14507b131f7df075dc199267dff1d9c4a1dc1
chung <souffle1903@gmail.com>
2024-08-26 20:27:50 +0900

    fix : 민경이 CORS 허용

--- backend/src/main/java/popz/solpop/config/WebConfig.java
+++ backend/src/main/java/popz/solpop/config/WebConfig.java
@@ -15,8 +15,9 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                         .allowedOriginPatterns("http://localhost:80",
-                                "http://localhost:3000", "http://localhost:5173", 
-                                "http://localhost:4173", "https://solpop.xyz") // 허용할 출처
+                                "http://localhost:3000", "http://localhost:5173",
+                                "http://localhost:4173", "https://solpop.xyz",
+                                "http://172.30.1.89:5173/") // 허용할 출처
                         .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                         .allowedHeaders("*")
                         .allowCredentials(true)
