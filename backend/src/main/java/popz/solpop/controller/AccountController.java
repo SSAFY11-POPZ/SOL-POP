1b66da53c538e23c1dc5f6b79421d74dbd2eb50a
chung <souffle1903@gmail.com>
2024-08-27 00:49:30 +0900

    feat : 계좌 잔액 조회 및 포인트충전(계좌출금) 기능

--- backend/src/main/java/popz/solpop/controller/AccountController.java
+++ backend/src/main/java/popz/solpop/controller/AccountController.java
@@ -3,14 +3,45 @@ package popz.solpop.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import popz.solpop.dto.SSAFYUserRequest;
+import popz.solpop.dto.SSAFYUserResponse;
+import popz.solpop.service.AccountService;
+import popz.solpop.service.AuthService;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
-//@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/account")
 public class AccountController {
 
     private static final Logger logger = LoggerFactory.getLogger(AccountController.class);
 
+    @Autowired
+    AccountService accountService;
+
+    @PostMapping("/checkAccountNo")
+    public Map<String, Object> postCheckAccountNo(
+            @RequestBody Map<String, Object> requestBody) {
+        try {
+            return accountService.checkAccountNo(requestBody);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/withdrawal")
+    public Map<String, Object> postWithdrawal(
+            @RequestBody Map<String, Object> requestBody) {
+        try {
+            return accountService.withdrawal(requestBody);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+    }
 
 }
--- backend/src/main/java/popz/solpop/service/AccountService.java
+++ backend/src/main/java/popz/solpop/service/AccountService.java
@@ -1,15 +1,67 @@
 package popz.solpop.service;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClient;
 import popz.solpop.repository.AccountRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+
+@Slf4j
 @Service
 @Transactional
 public class AccountService {
+  private static final Logger logger = LoggerFactory.getLogger(AccountService.class);
+
 
   @Autowired
   private AccountRepository accountRepository;
+
+
+  private WebClient webClient;
+
+  @PostConstruct
+  public void initWebClient() {
+      webClient = WebClient.create("https://finopenapi.ssafy.io/ssafy/api/v1");
+  }
+
+  public Map checkAccountNo(Map<String, Object> requestBody) throws Exception {
+    try {
+      return webClient.post()
+              .uri("/edu/demandDeposit/inquireDemandDepositAccountBalance")
+              .bodyValue(requestBody)
+              .retrieve()
+              .bodyToMono(Map.class)
+              .block();
+    } catch (Exception e) {
+      logger.error("SSAFY inquireDemandDepositAccountBalance failed");
+      System.out.println(e);
+      throw new Exception("SSAFY inquireDemandDepositAccountBalance failed", e);
+    }
+  }
+
+    //https://finopenapi.ssafy.io/ssafy/api/v1/edu/demandDeposit/updateDemandDepositAccountWithdrawal
+    public Map withdrawal(Map<String, Object> requestBody) throws Exception {
+      try {
+        return webClient.post()
+                .uri("/edu/demandDeposit/updateDemandDepositAccountWithdrawal")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(Map.class)
+                .block();
+      } catch (Exception e) {
+        logger.error("SSAFY AccountWithdrawal failed");
+        System.out.println(e);
+        throw new Exception("SSAFY AccountWithdrawal failed", e);
+      }
+  }
+
+
 
 }
