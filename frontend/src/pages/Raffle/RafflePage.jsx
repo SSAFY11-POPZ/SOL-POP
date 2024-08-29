74f614a73eb355055e5009e66e18c001f372098d
trick0846 <trick0846@naver.com>
2024-08-29 11:36:47 +0900

    fix : Raffle Page 스크롤 위치 오류 수정

--- frontend/src/pages/Raffle/RafflePage.jsx
+++ frontend/src/pages/Raffle/RafflePage.jsx
@@ -1,21 +1,24 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom'
-import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+import api from '../../utils/axios'
 
 import UpperBar from "../../components/UpperBar";
 import RaffleBanner from '../../assets/RaffleImg/RaffleBanner.png';
-import RaffleTest from '../../assets/RaffleImg/RaffleTest.png';
 import RaffleCard from "./components/RaffleCard";
 
 
 const RafflePage = () => {
   const [raffles, setRaffles] = useState(null);
-
+  const mainRef = useRef(null)
   useEffect(() => {
-    const fetchData = () => {
-      axios({
-        method:'get',
-        url:"https://popz.life/api/v1/raffle",
+    const ScrollToDiv = () => {
+      // 참조된 div가 있으면 그 위치로 스크롤 이동
+      if (mainRef.current) {
+        mainRef.current.scrollIntoView();
+      }
+    };
+    const fetchData = async () => {
+      // 래플 전체 리스트 조회 함수
+      api.get("/api/v1/raffle",{
       })
       .then(response => {
         console.log("성공완료!", response.data)
@@ -25,23 +28,22 @@ const RafflePage = () => {
         console.log(error)
       })
     };
+    ScrollToDiv();
     fetchData();
   },[]);
 
-  // axios 함수 작성 필요
   return (
-    <div className="my-3 min-h-lvh">
+    <div className="my-3 min-h-lvh" ref={mainRef}>
       <UpperBar />
       <img src={RaffleBanner} alt="래플페이지 배너" className="aspect-[1/1] w-full my-3 "/>
       <div>
         { raffles && raffles.length !=0 ? raffles.map((raffle) => (
           <RaffleCard key={raffle.raffleId} raffle={raffle} />
         )) : 
-        <div className="h-72 text-xl flex items-center justify-center text-stone-700">
+        <div className="flex items-center justify-center text-xl h-72 text-stone-700">
           <p>현재 진행중인 래플이 없습니다.</p>
         </div>}
       </div>
-      {/* navbar에 의해 가려지는 부분 처리하기! */}
       <div className="h-[100px]"></div>
     </div>
   );
