66580bf809463143f4743d70acba253ba4589a31
trick0846 <trick0846@naver.com>
2024-08-31 02:46:02 +0900

    fix : accessToken 시간 설정

--- backend/src/main/java/popz/solpop/controller/AuthController.java
+++ backend/src/main/java/popz/solpop/controller/AuthController.java
@@ -100,7 +100,7 @@ public class AuthController {
 
         Date issuedAt = (Date) tokenData.get("issuedAt");
         long elapsedSeconds = (new Date().getTime() - issuedAt.getTime()) / 1000;
-        int accessTokenDuration = 3600;
+        int accessTokenDuration = 7200;
 
         if (elapsedSeconds > accessTokenDuration) {
             return Response.setFailed("엑세스토큰이 만료되었습니다.");
@@ -149,7 +149,7 @@ public class AuthController {
             return Response.setFailed("리프레시 토큰이 유효하지 않습니다.");
         }
 
-        int accessTokenDuration = 3600;
+        int accessTokenDuration = 7200;
         String newAccessToken = tokenProvider.createAccessToken(userName, accessTokenDuration);
 
         if (newAccessToken == null) {
