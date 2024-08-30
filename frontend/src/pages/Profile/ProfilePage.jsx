f7b5c4acb49a5aab34b686ee99ccaed96e7b5c88
trick0846 <trick0846@naver.com>
2024-08-30 17:29:39 +0900

    fix : Auth, MainPage 수정

--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import ProfileSeeding from '../../assets/ProfileImg/ProfileSeeding.png'
-import ProfileMoney from '../../assets/ProfileImg/ProfileMoney.png'
-import Card from '../../assets/ProfileImg/Card.png'
-import Swal from 'sweetalert2'
-import api, { checkTokenValidity } from "../../utils/axios"
-import axios from 'axios'
-import { formatDateAndTime } from '../../utils/utils'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProfileSeeding from '../../assets/ProfileImg/ProfileSeeding.png';
+import ProfileMoney from '../../assets/ProfileImg/ProfileMoney.png';
+import Card from '../../assets/ProfileImg/Card.png';
+import Swal from 'sweetalert2';
+import api, { checkTokenValidity } from '../../utils/axios';
+import axios from 'axios';
+import { formatDateAndTime } from '../../utils/utils';
 
 const ProfilePage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   // 공통된 css
-  const hoverCss = "hover:bg-slate-300 hover:bg-opacity-20"
+  const hoverCss = 'hover:bg-slate-300 hover:bg-opacity-20';
 
-  const [balance, setBalance] = useState(0) // 계좌 잔액
-  const [point, setPoint] = useState(0) // 포인트
-  const [user, setUser] = useState({}) // 유저정보
-  const [amount, setAmount] = useState("") // 충전하기에서 입력한 금액
-  const [isModalOpen, setIsModalOpen] = useState(false) // 모달 오픈 여부
+  const [balance, setBalance] = useState(0); // 계좌 잔액
+  const [point, setPoint] = useState(0); // 포인트
+  const [user, setUser] = useState({}); // 유저정보
+  const [amount, setAmount] = useState(''); // 충전하기에서 입력한 금액
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 오픈 여부
   const [warning, setWarning] = useState('');
 
   // 1. 초기 렌더링시에 실행되는 함수
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (!localStorage.getItem("accessToken")) {
+        if (!localStorage.getItem('accessToken')) {
           // navigate("/login")
           return;
         }
@@ -34,7 +34,7 @@ const ProfilePage = () => {
 
         if (!response || response.status !== 200) {
           // navigate("/login");
-          console.log(response)
+          console.log(response);
           return;
         }
 
@@ -43,39 +43,41 @@ const ProfilePage = () => {
         //   return;
         // }
 
-  
         // 토큰이 유효하다면 반환된 데이터를 user에 할당
-        await setUser(response.data.data);
-  
+        setUser(response.data.data);
         // 포인트 조회
-        const pointResponse = await api.get("/api/v1/user/point/balance");
+        const pointResponse = await api.get('/api/v1/user/point/balance');
         setPoint(pointResponse.data);
-  
+
         // 계좌 잔액 조회
-        const { transmissionDate, transmissionTime, transmissionCode } = formatDateAndTime();
-        const balanceResponse = await api.post("api/v1/account/checkAccountNo", {
-          "Header": {
-            "apiName": "inquireDemandDepositAccountBalance",
-            "transmissionDate": transmissionDate,
-            "transmissionTime": transmissionTime,
-            "institutionCode": "00100",
-            "fintechAppNo": "001",
-            "apiServiceCode": "inquireDemandDepositAccountBalance",
-            "institutionTransactionUniqueNo": transmissionCode,
-            "apiKey": import.meta.env.VITE_ADMIN_SECRET_KEY,
-            "userKey": response.data.data.userKey // 여기서 response.data.data.userKey를 사용
+        const { transmissionDate, transmissionTime, transmissionCode } =
+          formatDateAndTime();
+        const balanceResponse = await api.post(
+          'api/v1/account/checkAccountNo',
+          {
+            Header: {
+              apiName: 'inquireDemandDepositAccountBalance',
+              transmissionDate: transmissionDate,
+              transmissionTime: transmissionTime,
+              institutionCode: '00100',
+              fintechAppNo: '001',
+              apiServiceCode: 'inquireDemandDepositAccountBalance',
+              institutionTransactionUniqueNo: transmissionCode,
+              apiKey: import.meta.env.VITE_ADMIN_SECRET_KEY,
+              userKey: response.data.data.userKey, // 여기서 response.data.data.userKey를 사용
+            },
+            accountNo: response.data.data.accountNo, // response.data.data.accountNo를 사용
           },
-          "accountNo": response.data.data.accountNo // response.data.data.accountNo를 사용
-        });
-  
+        );
+
         setBalance(balanceResponse.data.REC.accountBalance);
       } catch (error) {
-        console.error("Error fetching data:", error);
+        console.error('Error fetching data:', error);
         Swal.fire('ERROR', '오류가 발생했습니다.', 'warning');
-        navigate("/login")
+        navigate('/login');
       }
     };
-  
+
     fetchData();
   }, []);
 
@@ -88,7 +90,7 @@ const ProfilePage = () => {
   // 포인트 충전하기 모달 닫기
   const closeModal = () => {
     setIsModalOpen(false);
-    setAmount("")
+    setAmount('');
     setWarning('');
   };
 
@@ -133,113 +135,147 @@ const ProfilePage = () => {
   const chargePoint = async () => {
     try {
       // 계좌 출금 신청
-      const { transmissionDate, transmissionTime, transmissionCode } = formatDateAndTime();
-      await axios.post("https://solpop.xyz/api/v1/account/withdrawal", {
-        "Header": {
-          "apiName": "updateDemandDepositAccountWithdrawal",
-          "transmissionDate": transmissionDate,
-          "transmissionTime": transmissionTime,
-          "institutionCode": "00100",
-          "fintechAppNo": "001",
-          "apiServiceCode": "updateDemandDepositAccountWithdrawal",
-          "institutionTransactionUniqueNo": transmissionCode,
-          "apiKey": import.meta.env.VITE_ADMIN_SECRET_KEY,
-          "userKey": user.userKey
-        },
-        "accountNo": user.accountNo,
-        "transactionBalance":`${amount}`,
-        "transactionSummary":"(수시입출금) : 출금"
-      }).then(() => {
-        setBalance((prev) => prev-amount)
-      })
-      // 포인트 충전
-      await api.put(`/api/v1/point/charge?amount=${amount}`).then(() => {
-      }).then(() => {
-        closeModal()
-        Swal.fire({
-          title:"충전완료",
-          icon:"success",
-          html:"충전이 완료되었습니다. <br> 다양한 콘텐츠를 즐겨보세요!"
+      const { transmissionDate, transmissionTime, transmissionCode } =
+        formatDateAndTime();
+      await axios
+        .post('https://solpop.xyz/api/v1/account/withdrawal', {
+          Header: {
+            apiName: 'updateDemandDepositAccountWithdrawal',
+            transmissionDate: transmissionDate,
+            transmissionTime: transmissionTime,
+            institutionCode: '00100',
+            fintechAppNo: '001',
+            apiServiceCode: 'updateDemandDepositAccountWithdrawal',
+            institutionTransactionUniqueNo: transmissionCode,
+            apiKey: import.meta.env.VITE_ADMIN_SECRET_KEY,
+            userKey: user.userKey,
+          },
+          accountNo: user.accountNo,
+          transactionBalance: `${amount}`,
+          transactionSummary: '(수시입출금) : 출금',
         })
-        setPoint((prev) => prev + amount)
-      })
+        .then(() => {
+          setBalance((prev) => prev - amount);
+        });
+      // 포인트 충전
+      await api
+        .put(`/api/v1/point/charge?amount=${amount}`)
+        .then(() => {})
+        .then(() => {
+          closeModal();
+          Swal.fire({
+            title: '충전완료',
+            icon: 'success',
+            html: '충전이 완료되었습니다. <br> 다양한 콘텐츠를 즐겨보세요!',
+          });
+          setPoint((prev) => prev + amount);
+        });
     } catch {
       Swal.fire('ERROR', '오류가 발생했습니다.', 'warning');
-
     }
+  };
 
-  }
-  
   // 3. 내 계좌 잔액
   const showBalance = () => {
     const formattedAccountNo = user.accountNo.replace(/(\d{4})(?=\d)/g, '$1-');
-  
+
     Swal.fire({
       imageUrl: Card,
       imageWidth: 260,
       imageHeight: 150,
-      imageAlt: "Custom image",
-      html: `<span style="font-size:16px">${formattedAccountNo}</span><br><br>현재 잔액은 <strong>${balance}원</strong>입니다.`
+      imageAlt: 'Custom image',
+      html: `<span style="font-size:16px">${formattedAccountNo}</span><br><br>현재 잔액은 <strong>${balance}원</strong>입니다.`,
     });
-  }
+  };
 
   // 4. 내 찜한 목록
   const showWishlist = () => {
-    navigate("/profile/wishlist")
-  }
+    navigate('/profile/wishlist');
+  };
 
   // 5. 내 예약 목록
   const goToReservation = () => {
-    navigate("/profile/reservation")
-  }
+    navigate('/profile/reservation');
+  };
 
   return (
-    <div className="bg-[#f7f8fc] px-4 py-4 flex flex-col gap-2.5 h-screen">
-      <div className="bg-[#4472ea] p-5 rounded-[20px] flex-row w-full gap-2.5">
+    <div className="flex h-screen flex-col gap-2.5 bg-[#f7f8fc] px-4 py-4">
+      <div className="w-full flex-row gap-2.5 rounded-[20px] bg-[#4472ea] p-5">
         <div className="flex gap-3 p-3">
-          <div className="w-16 aspect-[1/1]">
+          <div className="aspect-[1/1] w-16">
             <img src={ProfileSeeding} alt="Profile Seeding" />
           </div>
           <div className="flex-row justify-end align-bottom">
-            <span className="block text-white text-lg font-bold font-['Noto Sans']">{user.name} 님,</span>
-            <span className="block text-white text-sm font-bold font-['Noto Sans']">
+            <span className="font-['Noto Sans'] block text-lg font-bold text-white">
+              {user.name} 님,
+            </span>
+            <span className="font-['Noto Sans'] block text-sm font-bold text-white">
               오늘도 쏠쏠한 혜택을 누리세요!
             </span>
           </div>
         </div>
-        <div className="bg-white p-2.5 rounded-[20px] flex flex-col items-center">
-          <div className="w-full px-5 py-2.5 flex items-center">
+        <div className="flex flex-col items-center rounded-[20px] bg-white p-2.5">
+          <div className="flex w-full items-center px-5 py-2.5">
             <div className="text-[#909090]">SOL Point</div>
           </div>
-          <div className="w-full flex justify-between items-center px-2.5 font-bold">
+          <div className="flex w-full items-center justify-between px-2.5 font-bold">
             <div className="flex items-center gap-2.5">
-              <img className="w-8 aspect-[1/1]" src={ProfileMoney} alt="Profile Money" />
+              <img
+                className="aspect-[1/1] w-8"
+                src={ProfileMoney}
+                alt="Profile Money"
+              />
               <div className="text-xl text-black">{point}원</div>
             </div>
-            <button className="text-[#4472ea]" onClick={openModal}>충전하기</button>
+            <button className="text-[#4472ea]" onClick={openModal}>
+              충전하기
+            </button>
           </div>
         </div>
       </div>
-      <div className="bg-white py-3 px-4 rounded-t-[20px] h-full w-full flex flex-col gap-y-2">
+      <div className="flex h-full w-full flex-col gap-y-2 rounded-t-[20px] bg-white px-4 py-3">
         <div className="flex flex-col gap-y-3">
           <p className="p-1 text-slate-500">조회</p>
-          <p className={`text-base ${hoverCss} p-1`} onClick={() => showBalance()}>내 계좌 잔액</p>
-          <p className={`text-base ${hoverCss} p-1`} onClick={() => showWishlist()}>내 찜한 목록</p>
-          <p className={`text-base ${hoverCss} p-1`} onClick={() => goToReservation()}>내 예약목록</p>
+          <p
+            className={`text-base ${hoverCss} p-1`}
+            onClick={() => showBalance()}
+          >
+            내 계좌 잔액
+          </p>
+          <p
+            className={`text-base ${hoverCss} p-1`}
+            onClick={() => showWishlist()}
+          >
+            내 찜한 목록
+          </p>
+          <p
+            className={`text-base ${hoverCss} p-1`}
+            onClick={() => goToReservation()}
+          >
+            내 예약목록
+          </p>
         </div>
       </div>
 
       {/* 모달 창 */}
       {isModalOpen && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={handleOverlayClick}>
-          <div className="relative w-4/5 max-w-lg p-6 bg-white rounded-lg" onClick={(e) => e.stopPropagation()}>
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleOverlayClick}
+        >
+          <div
+            className="relative w-4/5 max-w-lg p-6 bg-white rounded-lg"
+            onClick={(e) => e.stopPropagation()}
+          >
             <button
-              className="absolute text-gray-500 top-2 right-2"
+              className="absolute text-gray-500 right-2 top-2"
               onClick={closeModal}
             >
               X
             </button>
-            <h2 className="mb-4 text-xl font-semibold text-center">포인트 충전</h2>
+            <h2 className="mb-4 text-xl font-semibold text-center">
+              포인트 충전
+            </h2>
             <p className="mb-4 text-sm text-center text-gray-500">
               잔액 : {balance}원
             </p>
@@ -258,10 +294,12 @@ const ProfilePage = () => {
               />
             </div>
 
-            {warning && <p className="mb-4 text-center text-red-500">{warning}</p>}
+            {warning && (
+              <p className="mb-4 text-center text-red-500">{warning}</p>
+            )}
 
             <button
-              className={`w-full py-3 font-semibold text-white rounded-lg ${warning !== '' || amount === 0 ? 'bg-gray-400 cursor-not-allowed' : 'bg-yellow-400'}`}
+              className={`w-full rounded-lg py-3 font-semibold text-white ${warning !== '' || amount === 0 ? 'cursor-not-allowed bg-gray-400' : 'bg-yellow-400'}`}
               onClick={() => chargePoint()}
               disabled={warning !== '' || amount === 0}
             >
